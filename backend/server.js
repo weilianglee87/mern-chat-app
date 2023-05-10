6f449a87d7d625b2469ecf3f4a0863f54142e36e
weilianglee87 <weiliang.gsmedia@gmail.com>
2023-05-10 19:44:54 +0800

    backend registeruser done

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,20 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 
 connectDB();
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API is Running");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  const singleChat = chats.find((chat) => chat._id === req.params.id);
-  res.send(singleChat);
-});
+app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
