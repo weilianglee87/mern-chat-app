8e5ef2ad1fad1b7caf8598604e2149b46b8bbc82
weilianglee87 <weiliang.gsmedia@gmail.com>
2023-05-10 18:24:19 +0800

    create login and signup page

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,7 +1,52 @@
 import React from "react";
+import {
+  Container,
+  Box,
+  Text,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
 
-const Homepage = () => {
-  return <div>Homepage</div>;
-};
+function Homepage() {
+  return (
+    <Container maxW='xl' centerContent>
+      <Box
+        display='flex'
+        justifyContent='center'
+        p={3}
+        bg='white'
+        w='100%'
+        m='40px 0 15px 0'
+        borderRadius='lg'
+        borderWidth='1px'
+      >
+        <Text fontSize='4xl' fontFamily='Work sans'>
+          Mern Chat
+        </Text>
+      </Box>
+      <Box bg='white' w='100%' p={4} borderRadius='lg' borderWidth='1px'>
+        <Tabs isFitted variant='soft-rounded'>
+          <TabList mb='1em'>
+            <Tab width='50%'>Login</Tab>
+            <Tab width='50%'>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+}
 
 export default Homepage;
