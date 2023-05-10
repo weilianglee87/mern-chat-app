1e700d5d26bff134e1f8c5e8bf40fa8b5ac2c359
weilianglee87 <weiliang.gsmedia@gmail.com>
2023-05-11 02:35:44 +0800

    setup chatpage

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -1,26 +1,17 @@
-import React from "react";
-// import axios from "axios";
-// import { useState, useEffect } from "react";
+import { Box } from "@chakra-ui/layout";
+import { ChatState } from "../Context/ChatProvider";
 
 const ChatPage = () => {
-  // const [chats, setChats] = useState([]);
-  // const fetchChats = async () => {
-  //   const { data } = await axios.get("/api/chat");
-
-  //   setChats(data);
-  // };
-
-  // useEffect(() => {
-  //   fetchChats();
-  // }, []);
+  const { user } = ChatState();
 
   return (
-    <div>
-      {/* {chats.map((chat) => (
-        <div key={chat._id}>{chat.chatName}</div>
-      ))} */}
+    <div style={{ width: "100%" }}>
+      {/* {user && <SideDrawer />} */}
+      <Box>
+        {/* {user %% >MyChats/>} */}
+        {/* {user %% >ChatBox/>} */}
+      </Box>
     </div>
   );
 };
-
 export default ChatPage;
