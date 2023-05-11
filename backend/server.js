a59a9709ccf8b9dc69a7847708ecbff5a851ffc4
weilianglee87 <weiliang.gsmedia@gmail.com>
2023-05-11 23:57:59 +0800

    fix bug

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.URL,
   },
 });
 
