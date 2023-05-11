3424d516eb201b271ace3a087954d8fedd52e55f
weilianglee87 <weiliang.gsmedia@gmail.com>
2023-05-11 15:47:44 +0800

    touch up controller folder

--- backend/server.js
+++ backend/server.js
@@ -28,4 +28,43 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server Started on PORT ${PORT}`));
+const server = app.listen(PORT, console.log(`Server Started on PORT ${PORT}`));
+
+const io = require("socket.io")(server, {
+  pingTimeout: 6000,
+  cors: {
+    orgin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User joined chat: " + room);
+  });
+
+  socket.on("new message", (newMessageReceived) => {
+    let chat = newMessageReceived.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageReceived.sender._id) return;
+
+      socket.in(user._id).emit("message received", newMessageReceived);
+    });
+  });
+  socket.on("typing", (room) => {
+    socket.in(room).emit("typing");
+  });
+  socket.on("stop typing", (room) => {
+    socket.in(room).emit("stop typing");
+  });
+});
