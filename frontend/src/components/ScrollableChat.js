c52bbb86ed5137b398da21999823de6325e0b2cd
weilianglee87 <weiliang.gsmedia@gmail.com>
2023-05-11 20:10:35 +0800

    fix some bug not add time in chat

--- frontend/src/components/ScrollableChat.js
+++ frontend/src/components/ScrollableChat.js
@@ -8,70 +8,52 @@ import {
 } from "../config/ChatLogics";
 import { ChatState } from "../Context/ChatProvider";
 import { Avatar, Tooltip } from "@chakra-ui/react";
-import moment from "moment";
 
 const ScrollableChat = ({ messages }) => {
   const { user } = ChatState();
+  console.log(messages);
   return (
     <ScrollableFeed>
-      {messages.map((m, i) => {
-        const dayAfter = moment(messages[i + 1]?.createdAt).format(
-          "Do MMM YYYY"
-        );
-        const sendDate = moment(m.createdAt).format("Do MMM YYYY");
-
-        return (
-          <div key={m._id}>
-            {dayAfter !== sendDate && (
-              <div style={{ textAlign: "center" }}>
-                <p>{sendDate}</p>
-                <br />
-              </div>
-            )}
-            <div style={{ display: "flex" }}>
-              {(isSameSender(messages, m, i, user._id) ||
-                isLastMessage(messages, i, user._id)) && (
-                <Tooltip
-                  label={m.sender.name}
-                  placement='bottom-start'
-                  hasArrow
-                >
-                  <Avatar
-                    mt='7px'
-                    mr={1}
-                    size='sm'
-                    name={m.sender.name}
-                    src={m.sender.pic}
-                  />
-                </Tooltip>
-              )}
-              <Tooltip
-                label={new Date(m.createdAt).toLocaleTimeString([], {
-                  hour: "2-digit",
-                  minute: "2-digit",
-                })}
-                placement='bottom'
-                hasArrow
-              >
-                <span
-                  style={{
-                    backgroundColor: `${
-                      m.sender._id === user._id ? "#BEE3F8" : "#B9F5D0"
-                    }`,
-                    borderRadius: "20px",
-                    padding: "5px 15px",
-                    maxWidth: "75%",
-                    marginLeft: isSameSenderMargin(messages, m, i, user._id),
-                    marginTop: isSameUser(messages, m, i, user._id) ? 3 : 10,
-                  }}
-                >
-                  {m.content}
-                </span>
+      {messages &&
+        messages.map((m, i) => (
+          <div style={{ display: "flex" }} key={m._id}>
+            {(isSameSender(messages, m, i, user._id) ||
+              isLastMessage(messages, i, user._id)) && (
+              <Tooltip label={m.sender.name} placement='bottom-start' hasArrow>
+                <Avatar
+                  mt='7px'
+                  mr={1}
+                  size='sm'
+                  name={m.sender.name}
+                  src={m.sender.pic}
+                />
               </Tooltip>
-            </div>
+            )}
+            <Tooltip
+              label={new Date(m.createdAt).toLocaleTimeString([], {
+                hour: "2-digit",
+                minute: "2-digit",
+              })}
+              placement='bottom'
+              hasArrow
+            >
+              <span
+                style={{
+                  backgroundColor: `${
+                    m.sender._id === user._id ? "#BEE3F8" : "#B9F5D0"
+                  }`,
+                  borderRadius: "20px",
+                  padding: "5px 15px",
+                  maxWidth: "75%",
+                  marginLeft: isSameSenderMargin(messages, m, i, user._id),
+                  marginTop: isSameUser(messages, m, i, user._id) ? 3 : 10,
+                }}
+              >
+                {m.content}
+              </span>
+            </Tooltip>
           </div>
-        );
-      })}
+        ))}
     </ScrollableFeed>
   );
 };
