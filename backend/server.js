a803ea4b08feef6709bee637a1829292b170628b
weilianglee87 <weiliang.gsmedia@gmail.com>
2023-05-12 18:37:24 +0800

    fix bug

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,14 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(express.json());
 
