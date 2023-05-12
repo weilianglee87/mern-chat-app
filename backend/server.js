bc6009ef6e9da01a62a351013c76265a2247b5f0
weilianglee87 <weiliang.gsmedia@gmail.com>
2023-05-12 18:32:55 +0800

    add cors to socket.io

--- backend/server.js
+++ backend/server.js
@@ -56,6 +56,8 @@ const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
     origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
     credentials: true,
   },
 });
